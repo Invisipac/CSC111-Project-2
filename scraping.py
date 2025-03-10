909a9acaa702ddf24ca9cb03cd94241cd1c469ef
Grape <0707bhd@gmail.com>
2025-03-09 21:00:41 -0400

    make scrape function return dicts

--- scraping.py
+++ scraping.py
@@ -5,12 +5,14 @@ import pprint
 test_url = "/wiki/Computer_science"
 
 
-def get_hyperlinks(page_url: str) -> list[str]:
+def get_hyperlinks(page_url: str) -> dict:
     url_base = "https://en.wikipedia.org"
     page = requests.get(url_base + page_url)
 
     soup = BeautifulSoup(page.content, "html.parser")
     links = soup.find_all('a')
+
+    word_filter = {':', 'Glossary', 'History_of', 'List_of', 'Timeline_of', '_'}
 
     usable_links = []
     for link in links:
@@ -21,10 +23,11 @@ def get_hyperlinks(page_url: str) -> list[str]:
                 ('/wiki/' in url) and
                 (url not in page_url) and
                 (':' not in url) and
-                ('Glossary' not in url)):
+                (all(word not in url for word in word_filter))):
             usable_links.append(url)
 
-    return sorted(usable_links)
+    usable_links.sort()
+    return {link: dict() for link in usable_links}
 
 
 pprint.pprint(get_hyperlinks(test_url))
