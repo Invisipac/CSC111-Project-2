5a75aec90217c50f93037e4585677462c4cb8811
Teppi Le <teppipersonal@gmail.com>
2025-03-29 20:45:33 -0400

    updated all basic functio

--- tkinter_elements.py
+++ tkinter_elements.py
@@ -11,10 +11,21 @@ class AutocompleteText(tk.Text):
 
         # special handling for tab, which needs to happen on the
         # key _press_
-        self.bind("<Tab>", self._handle_space)
+        self.bind("<Tab>", self._handle_tab)
 
-    def _handle_space(self, event):
-        pass
+    def _handle_tab(self, event):
+        # see if any text has the "autocomplete" tag
+        tag_ranges= self.tag_ranges("autocomplete")
+        if tag_ranges:
+            # move the insertion cursor to the end of
+            # the selected text, and then remove the "sel"
+            # and "autocomplete" tags
+            self.mark_set("insert", tag_ranges[1])
+            self.tag_remove("sel", "1.0", "end")
+            self.tag_remove("autocomplete", "1.0", "end")
+
+            # prevent the default behavior of inserting a literal tab
+            return "break"
 
     def _autocomplete(self, event):
         if event.char and self.callback:
@@ -38,12 +49,3 @@ class AutocompleteText(tk.Text):
 
                 # move the cursor back to the saved position
                 self.mark_set("insert", insert)
-
-
-def get_matches(word):
-    # For illustrative purposes, pull possible matches from
-    # what has already been typed. You could just as easily
-    # return a list of pre-defined keywords.
-    words = text.get("1.0", "end-1c").split()
-    matches = [x for x in words if x.startswith(word)]
-    return matches
